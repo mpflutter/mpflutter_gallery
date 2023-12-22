0dfba1941de37cf1ffc45428ad9b5f99d83aebc2
崔 明辉 <cuis@vip.qq.com>
2023-12-23 00:22:29 +0800

    完善 map 测试用例

--- lib/ext/wx_map_view.dart
+++ lib/ext/wx_map_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mpflutter_wechat_api/mpflutter_wechat_api.dart';
 import 'package:mpflutter_wechat_mapview/mpflutter_wechat_mapview.dart';
 
 class WXMapView extends StatefulWidget {
@@ -10,14 +11,15 @@ class WXMapView extends StatefulWidget {
 
 class _WXMapViewState extends State<WXMapView> {
   final controller = MPFlutter_Wechat_MapViewController();
+  String selectedAddress = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('WXVideoView'),
+        title: const Text('WXMapView'),
       ),
-      body: ListView(
+      body: Column(
         children: [
           SizedBox(
             height: 400,
@@ -25,14 +27,51 @@ class _WXMapViewState extends State<WXMapView> {
               controller: controller,
               longitude: 113.347698,
               latitude: 23.100022,
+              showLocation: true,
+              markers: const [
+                {
+                  "id": "HeJi",
+                  "title": "赫基大厦",
+                  "longitude": 113.347698,
+                  "latitude": 23.100022,
+                },
+                {
+                  "id": "WeChat",
+                  "title": "微信总部",
+                  "longitude": 113.324560,
+                  "latitude": 23.100100,
+                }
+              ],
+              onMarkerTap: (detail) {
+                setState(() {
+                  selectedAddress = detail["markerId"];
+                });
+              },
             ),
           ),
-          ListTile(
-            title: const Text('Push Next'),
-            onTap: () {
-              Navigator.of(context).pushNamed("/ui_waterfall_view");
-            },
-          ),
+          Expanded(
+            child: ListView(
+              children: [
+                ListTile(
+                  title: Text('当前选中的地点是: $selectedAddress'),
+                ),
+                const Divider(),
+                ListTile(
+                  title: const Text('To > 微信总部'),
+                  onTap: () {
+                    final mapContext = controller.getMapContext();
+                    if (mapContext != null) {
+                      mapContext.moveToLocation(
+                        MoveToLocationOption()
+                          ..latitude = 23.100100
+                          ..longitude = 113.324560,
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );

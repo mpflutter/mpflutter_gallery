05bf9087ff0e910737ad5788c76361406754d162
崔 明辉 <cuis@vip.qq.com>
2024-01-15 21:56:01 +0800

    feat: add multiple button test

--- lib/ext/wx_button.dart
+++ lib/ext/wx_button.dart
@@ -16,18 +16,33 @@ class _WXButtonState extends State<WXButton> {
         title: const Text('WXButton'),
       ),
       body: Center(
-        child: MPFlutter_Wechat_Button(
-          openType: "getUserInfo",
-          onGetUserInfo: (detail) {
-            final snakeBar = SnackBar(
-              content: Text(detail["userInfo"]["nickName"]),
-            );
-            ScaffoldMessenger.of(context).showSnackBar(snakeBar);
-          },
-          child: MaterialButton(
-            onPressed: () {},
-            child: const Text('Get User Info'),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MPFlutter_Wechat_Button(
+              openType: "getUserInfo",
+              onGetUserInfo: (detail) {
+                final snakeBar = SnackBar(
+                  content: Text(detail["userInfo"]["nickName"]),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snakeBar);
+              },
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.blue,
+                child: const Text('Get User Info'),
+              ),
+            ),
+            const SizedBox(height: 44),
+            MPFlutter_Wechat_Button(
+              openType: "openSetting",
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.blue,
+                child: const Text('Open Setting'),
+              ),
+            )
+          ],
         ),
       ),
     );

5002fcce3af3c87a47e5a5b7892e7f3bb8242a8b
崔 明辉 <cuis@vip.qq.com>
2024-06-23 16:41:28 +0800

    feat: add minitex build script douyin.

--- scripts/build_douyin.dart
+++ scripts/build_douyin.dart
@@ -3,5 +3,6 @@ import 'package:mpflutter_build_tools/main.dart' as build_tools;
 void main(List<String> arguments) async {
   final buildArgs = [...arguments]..add('--douyin');
   build_tools.useMiniTex = true;
+  build_tools.useNoFontCanvasKit = true;
   build_tools.main(buildArgs);
 }
